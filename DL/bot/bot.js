535ee6849410cccea105cefa42d5ad9f2f6e27e2
oz-tzachor <ozi380@gmail.com>
2022-08-15 15:59:50 +0300

    make it better

--- DL/bot/bot.js
+++ DL/bot/bot.js
@@ -3,16 +3,15 @@ const { newMessage } = require("./messages");
 const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
 const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 ////////
-const sendMessage = (chatId, message) => {
+exports.sendMessage = (chatId, message) => {
   let messageToSent = ``;
   bot.sendMessage(chatId, message);
   return;
 };
-let dealWithMessage = async () => {
+exports.dealWithMessage = async () => {
   bot.on("message", async (msg) => {
     let chatId = msg.chat.id;
     let text = msg.text;
     newMessage(chatId, text, sendMessage);
   });
 };
-module.exports = { dealWithMessage, sendMessage };
